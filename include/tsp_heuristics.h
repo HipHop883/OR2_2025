a8d1aa056614b4a0b97590c694340bdaa8df7268
Andrea Bruttomesso <andrea.bruttomesso.1@studenti.unipd.it>
2025-03-22 17:46:22 +0100

    fix: typo

--- include/tsp_heuristics.h
+++ include/tsp_heuristics.h
@@ -1,9 +1,9 @@
-#ifndef VNS_H_
-#define VNS_H_
+#ifndef TSP_HEURISTICS_H_
+#define TSP_HEURISTICS_H_
 
 #include "tsp.h"
 #include "tsp_greedy.h"
 
 int tsp_solve_vns(instance *tsp, solution *sol);
 
-#endif // VNS_H_
+#endif // TSP_HEURISTICS_H_

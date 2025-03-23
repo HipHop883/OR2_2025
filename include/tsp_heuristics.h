b5ccaf2a119df97ff47b6e86c79873d8fb2f1948
Andrea Bruttomesso <andrea.bruttomesso.1@studenti.unipd.it>
2025-03-23 13:50:41 +0100

    move tabu into heuristics

--- include/tsp_heuristics.h
+++ include/tsp_heuristics.h
@@ -5,5 +5,8 @@
 #include "tsp_greedy.h"
 
 int tsp_solve_vns(instance *tsp, solution *sol);
+int tsp_solve_tabu(instance *inst, solution *sol);
+
+static int two_opt_random(instance *tsp, solution *sol, int **tabu_list);
 
 #endif // TSP_HEURISTICS_H_
